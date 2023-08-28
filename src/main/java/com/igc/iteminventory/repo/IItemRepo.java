8e98f3067d1c2d9bfd43b803f6c29fc2076a08ad
mayurmore14 <mayurmore8505@gmail.com>
2023-08-28 09:30:28 -0700

    MODIFIED: Update,Delete operations and Response Entity added

---
+++ src/main/java/com/igc/iteminventory/repo/IItemRepo.java
@@ -0,0 +1,9 @@
+package com.igc.iteminventory.repo;
+
+import com.igc.iteminventory.entity.ItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IItemRepo extends JpaRepository<ItemEntity, Integer> {
+}
