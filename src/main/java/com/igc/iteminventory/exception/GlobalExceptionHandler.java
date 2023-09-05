d5e2e96350331b4cbf1f3d3c7f28bd1df94655ab
mayurmore14 <mayurmore8505@gmail.com>
2023-09-05 02:04:41 -0700

    MODIFIED: Data Validation Added

--- src/main/java/com/igc/iteminventory/exception/GlobalExceptionHandler.java
+++ src/main/java/com/igc/iteminventory/exception/GlobalExceptionHandler.java
@@ -1,16 +1,35 @@
 package com.igc.iteminventory.exception;
 
 import com.igc.iteminventory.responses.Responses;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ItemNotFoundException.class)
     public ResponseEntity<?> itemNotFoundEx() {
         return new ResponseEntity<>(new Responses("Item With Given Id Not Found in Database", HttpStatus.NOT_FOUND),HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,String> errorMap = new HashMap<>();
+        for(FieldError field : ex.getFieldErrors()) {
+            errorMap.put(field.getField(),field.getDefaultMessage());
+            //logger.info(field.getField()+" = "+field.getDefaultMessage());
+        }
+        return new ResponseEntity<>(new Responses("Please Check the Input Data",HttpStatus.BAD_REQUEST,errorMap),HttpStatus.BAD_REQUEST);
     }
 }
